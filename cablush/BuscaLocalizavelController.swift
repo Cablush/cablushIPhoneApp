5549fb935b0f9c2165738fde1dbec207f951bc16
Jonathan <jhonnybat@gmail.com>
2016-03-22 17:29:23 -0300

    Controller

---
+++ cablush/BuscaLocalizavelController.swift
@@ -0,0 +1,17 @@
+//
+//  BuscaLocalizavelController.swift
+//  Cablush
+//
+//  Created by Jonathan on 15/02/16.
+//  Copyright © 2016 Cablush. All rights reserved.
+//
+
+import Foundation
+
+class BuscaLocalizavelController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
