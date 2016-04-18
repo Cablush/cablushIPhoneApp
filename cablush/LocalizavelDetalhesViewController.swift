5c61072491dfe836f6faf216c8aa0b6f0f670b0d
Jonathan <jhonnybat@gmail.com>
2016-04-18 13:06:05 -0300

    Localizável Page View Controller

--- cablush/LocalizavelDetalhesViewController.swift
+++ cablush/LocalizavelDetalhesViewController.swift
@@ -12,33 +12,70 @@ import UIKit
 
 
 class LocalizavelDetalhesViewController: UIViewController {
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var nome: UILabel!
-    @IBOutlet weak var detalhes: UITextView!
-    
-    @IBOutlet weak var site: UILabel!
-    @IBOutlet weak var facebook: UILabel!
-    @IBOutlet weak var endereco: UILabel!
-    @IBOutlet weak var telefone: UILabel!
-    @IBOutlet weak var email: UILabel!
-    
-    @IBOutlet weak var siteImg: UIImageView!
-    @IBOutlet weak var facebookImg: UIImageView!
-    @IBOutlet weak var enderecoImg: UIImageView!
-    @IBOutlet weak var emailImg: UIImageView!
-    @IBOutlet weak var telefoneImg: UIImageView!
     
     var localizavel = Localizavel()
+
+    @IBOutlet weak var videoImg: UIImageView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var enderecoImg: UIImageView!
+    @IBOutlet weak var facebookImg: UIImageView!
+    @IBOutlet weak var emailImg: UIImageView!
+    @IBOutlet weak var telImg: UIImageView!
+    @IBOutlet weak var websiteImg: UIImageView!
+    
+    
+    @IBOutlet weak var nome: UILabel!
+    @IBOutlet weak var detalhes: UITextView!
+    @IBOutlet weak var video: UIButton!
+    @IBOutlet weak var telefone: UIButton!
+    @IBOutlet weak var email: UIButton!
+    @IBOutlet weak var endereco: UIButton!
+    @IBOutlet weak var site: UIButton!
+    @IBOutlet weak var facebook: UIButton!
+    
+    var tel :String = ""
+    var fb :String = ""
+    var ws :String = ""
+    var mail :String = ""
+    var lat :String = ""
+    var lng :String = ""
+    var vid :String = ""
     
     override func viewDidLoad() {
+        fb = localizavel.facebook
+        ws = localizavel.website ?? "www.cablush.com"
         nome.text       = localizavel.nome
         detalhes.text   = localizavel.descricao
-        site.text       = localizavel.website
-        facebook.text   = localizavel.facebook
-        endereco.text   = "Endereco"
-        telefone.text   = "Telefone"
-        email.text      = "E-mail"
+        site.setTitle(localizavel.website, forState: UIControlState.Normal)
         
+        facebook.setTitle(localizavel.facebook, forState: UIControlState.Normal)
+        if let loja = localizavel as? Loja{
+            tel = loja.telefone
+            mail = loja.email
+            lat = "\(loja.locais[0].latitude)"
+            lng = "\(loja.locais[0].longitude)"
+            telefone.setTitle(loja.telefone, forState: UIControlState.Normal)
+            email.setTitle(loja.email, forState: UIControlState.Normal)
+            endereco.setTitle(loja.locais[0].toString(), forState: UIControlState.Normal)
+        }else if let pista = localizavel as? Pista{
+            lat = "\(pista.local.latitude)"
+            lng = "\(pista.local.longitude)"
+            
+            if pista.video.isEmpty{
+                video.hidden = true
+                videoImg.hidden = true
+            }else{
+                video.setTitle(pista.video, forState: .Normal)
+                vid = pista.video
+            }
+            
+            endereco.setTitle(pista.local.toString(), forState: UIControlState.Normal)
+        }else if let evento = localizavel as? Evento{
+            lat = "\(evento.local.latitude)"
+            lng = "\(evento.local.longitude)"
+            
+            endereco.setTitle(evento.local.toString(), forState: UIControlState.Normal)
+        }
         let url = NSURL(string:localizavel.img_url)
         downloadImage(url!)
     }
@@ -64,6 +101,36 @@ class LocalizavelDetalhesViewController: UIViewController {
         NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) in
             completion(data: data, response: response, error: error)
             }.resume()
+    }
+    
+    @IBAction func telefone(sender: AnyObject) {
+        
+        let trimmedString = tel.stringByReplacingOccurrencesOfString("\\s", withString: "", options: NSStringCompareOptions.RegularExpressionSearch, range: nil)
+        print("tel://\(trimmedString)")
+        let url = NSURL(string: "tel://\(trimmedString)")
+        
+        UIApplication.sharedApplication().openURL(url!)
+    }
+    
+    @IBAction func email(sender: AnyObject) {
+        let url = NSURL(string: "mailto:\(mail)")
+        UIApplication.sharedApplication().openURL(url!)
+    }
+    @IBAction func site(sender: AnyObject) {
+        let url = NSURL(string: "http://\(ws)")
+        UIApplication.sharedApplication().openURL(url!)
+    }
+    @IBAction func facebook(sender: AnyObject) {
+        let url = NSURL(string: "http://\(fb)")
+        UIApplication.sharedApplication().openURL(url!)
+    }
+    
+    @IBAction func endereco(sender: AnyObject) {
+
+    }
+    @IBAction func video(sender: AnyObject) {
+        let url = NSURL(string: "\(vid)")
+        UIApplication.sharedApplication().openURL(url!)
     }
     
 }
